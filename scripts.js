8c6988747d82a35818e2ef4fa5ec930358c44d53
Angelo <angelocsaverino@gmail.com>
2022-05-09 20:19:24 -0400

    added 10 questions

--- scripts.js
+++ scripts.js
@@ -8,29 +8,85 @@ var controlNumber = 0;
 
 var questionsArray = [
     {
-        text: "1+1=?",
-        option1: "4",
-        option2: "2",
-        option3: "0",
-        option4: "5",
-        correct: "2"
+        text: "Inside which HTML element do we put the JavaScript?",
+        option1: "<javascript>",
+        option2: "<js>",
+        option3: "<script>",
+        option4: "<scripting>",
+        correct: "<script>"
     },
     {
-        text: "1+3=?",
-        option1: "2",
-        option2: "0",
-        option3: "5",
-        option4: "4",
-        correct: "4"
+        text: "How do you create a function?",
+        option1: "function:myFunction()",
+        option2: "function=myFunction()",
+        option3: "function myFunction()",
+        option4: "myFunction():function",
+        correct: "function myFunction()"
     },
     {
-        text: "2+3=?",
-        option1: "5",
-        option2: "3",
-        option3: "2",
-        option4: "1",
-        correct: "5"
-    }
+        text: "How many looping statements are there in JavaScript?",
+        option1: "2. The for loop, and the while loop",
+        option2: "4. The for loop, the while loop, the do...while loop, and the loop...until loop",
+        option3: "3. The for loop, the while loop, and the do...while loop",
+        option4: "1. The for loop",
+        correct: "3. The for loop, the while loop, and the do...while loop"
+    },
+    {
+        text: "How can you add a comment in JavaScript?",
+        option1: "//This is a comment",
+        option2: "'This is a comment",
+        option3: "<!--This is a comment-->",
+        option4: "#This is a comment",
+        correct: "//This is a comment"
+    },
+    {
+        text: "How do you round the number 7.25, to the nearest whole number?",
+        option1: "Math.rnd(7.25)",
+        option2: "round(7.25)",
+        option3: "rnd(7.25)",
+        option4: "Math.round(7.25)",
+        correct: "Math.round(7.25)"
+    },
+    {
+        text: "In HTML, use the _____ property/attribute to set a default value that displays in an input box when the form is initially displayed.",
+        option1: "Default",
+        option2: "Value",
+        option3: "Form",
+        option4: "None of the above.",
+        correct: "Value"
+    },
+    {
+        text: "In JavaScript, the following loop will execute ________ times. for (x=1; x<11; x++)",
+        option1: "10",
+        option2: "9",
+        option3: "11",
+        option4: "cannot tell from this portion of the script",
+        correct: "10"
+    },
+    {
+        text: "The majority of a typical Web document will be found in:",
+        option1: "the head tag",
+        option2: "the title tag",
+        option3: "the body tag",
+        option4: "a comment tag",
+        correct: "the body tag"
+    },
+    {
+        text: "A named element in a JavaScript program that is used to store and retrieve data is a _____.",
+        option1: "method",
+        option2: "assignment operator",
+        option3: "variable",
+        option4: "string",
+        correct: "variable"
+    },
+    {
+        text: "In JavaScript, which of the following is a logical operator?",
+        option1: "|",
+        option2: "&&",
+        option3: "%",
+        option4: "/",
+        correct: "&&"
+    },
 ]
 
 const displayQuestion = () => {
