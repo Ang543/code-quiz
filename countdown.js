8923236960beed28e7b0f7f3c4e607aff881ff5a
Angelo <angelocsaverino@gmail.com>
2022-05-11 04:53:32 -0400

    fixed some timer issues and started styling

--- countdown.js
+++ countdown.js
@@ -5,12 +5,16 @@ const countdownEl = document.getElementById('countdown');
 
 setInterval(updateCountdown, 1000);
 
-function updateCountdown(){
+function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
-seconds = seconds < 10 ? `0` + seconds : seconds;
+    seconds = seconds < 10 ? `0` + seconds : seconds;
 
     countdownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
+
+    if (time < 0) { //stop the setInterval when time = 0 to avoid negative time
+        clearInterval(refreshIntervalId);
+    }
 }
