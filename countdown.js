db389ec68943cdc6bed30bd337d72906c0f15e2f
Angelo <angelocsaverino@gmail.com>
2022-05-11 19:07:56 -0400

    timer working stops at 0

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 const startingMinutes = 2;
 let time = startingMinutes * 60;
-
+// let time = 5;
 const countdownEl = document.getElementById('countdown');
 
 setInterval(updateCountdown, 1000);
@@ -11,10 +11,11 @@ function updateCountdown() {
 
     seconds = seconds < 10 ? `0` + seconds : seconds;
 
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    countdownEl.innerHTML = 
+    `${minutes}:${seconds}`;
     time--;
 
     if (time < 0) { //stop the setInterval when time = 0 to avoid negative time
-        clearInterval(refreshIntervalId);
+        countdownEl.innerHTML = 0;
     }
 }
